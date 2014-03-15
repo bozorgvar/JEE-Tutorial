3a340a05b4b2ba14f71505d619f96d989c4e3352
ammar <ammar.bozorgvar@gmail.com>
2014-03-15 18:33:23 +0330

    none scope and  view scope

---
+++ JSF/AnimalWeb/src/main/java/com/mycompany/animalweb/NoneBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.animalweb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.NoneScoped;
+
+/**
+ *
+ * @author Ammar Bozorgvar at
+ * http://stackoverflow.com/users/382154/ammar-bozorgvar
+ */
+@ManagedBean
+@NoneScoped
+public class NoneBean {
+
+    /**
+     * Creates a new instance of NoneBean
+     */
+    public NoneBean() {
+    }
+}
---
+++ JSF/AnimalWeb/src/main/java/com/mycompany/animalweb/ViewBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.animalweb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Ammar Bozorgvar at
+ * http://stackoverflow.com/users/382154/ammar-bozorgvar
+ */
+@ManagedBean
+@ViewScoped
+public class ViewBean {
+
+    /**
+     * Creates a new instance of ViewBean
+     */
+    public ViewBean() {
+    }
+}
